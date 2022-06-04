3fea24e801eec3d03eda2268e957dd4a8b58f770
Thomas Henriet <dabalx@gmail.com>
2022-06-04 15:52:08 +0200

    User controller, Devise config, Mapbox initate, Add dotenv - Protect my secret data in .env file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :zipcode, :player, :trader])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :zipcode, :player, :trader])
+  end
+
+  def after_sign_in_path_for(current_user)
+    user_path(current_user) # your path
+  end
 end
