823f818a9e56347ac9ca595c3615abf10426f706
Thomas Henriet <dabalx@gmail.com>
2022-06-04 12:18:08 +0200

    routes en place

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :bookings, except: [:index]
+  end
+  resources :articles
+  resources :chatrooms, only: [:show]
+  resources :messages, only: [:create]
 end
