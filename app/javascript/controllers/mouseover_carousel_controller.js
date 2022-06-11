fbf295d7787ceaef2c8c098caeb1c6239f79450c
Lucas Toschini <lucas.toschini@live.fr>
2022-06-11 15:40:34 +0200

    pas grand chose

---
+++ app/javascript/controllers/mouseover_carousel_controller.js
@@ -0,0 +1,15 @@
+// import { Controller } from "@hotwired/stimulus"
+
+// export default class extends Controller {
+//   static targets = ["item"]
+
+//   changeFirstCard() {
+//     console.log(this.element)
+//     this.element.classList.add("first-card")
+//   }
+
+//   changeSecondCard() {
+//     console.log(this.element)
+//     this.element.classList.add("second-card")
+//   }
+// }
