b069073b9c74abaf01936f277d2f1372854d7a3f
Ondine Debienne <ondine.debienne@gmail.com>
2022-06-21 18:45:04 +0200

    début booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,37 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.where(creator: current_user)
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @booking = Booking.new
+    @booking.creator = @user
+  end
+
+  def create
+    @booking = Booking.new(params_booking)
+    @booking.creator = @user
+    @booking.save
+    # redirect_to user_booking_path(@booking)
+  end
+
+  def update
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    # redirect_to bookings_path
+  end
+
+  private
+
+  def params_booking
+    params.require(:booking).permit(:player_id, :game_id, :booking_type, :date)
+  end
 end
--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -6,6 +6,7 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    @booking = Booking.new
   end
 
   def new
