f0bbb85487e9410dfe70d142d5fd897bfc66daec
Lucas Toschini <lucas.toschini@live.fr>
2022-06-23 22:43:24 +0200

    booking show + controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,10 +19,18 @@ class BookingsController < ApplicationController
     @booking.player = User.find(params[:booking][:player_id])
     @booking.creator = current_user
     @booking.save!
-    redirect_to user_booking_path(current_user.id, @booking.id)
+    redirect_to user_booking_path(current_user, @booking.id)
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    @booking.update(result: params[:booking][:result])
+    if @booking.result == current_user.username
+      current_user.update(player_score: current_user.player_score + 1)
+    else
+      @booking.player.update(player_score: @booking.player.player_score + 1)
+    end
+    redirect_to user_bookings_path(current_user)
   end
 
   def destroy
@@ -34,6 +42,6 @@ class BookingsController < ApplicationController
   private
 
   def params_booking
-    params.require(:booking).permit(:player_id, :game_id, :booking_type, :date)
+    params.require(:booking).permit(:player_id, :game_id, :booking_type, :date, :result)
   end
 end
