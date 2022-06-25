2a3fc234cb9ef652a5f73f16bce9634f3161a1d7
Ondine Debienne <ondine.debienne@gmail.com>
2022-06-25 14:16:53 +0200

    chatroom done

---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+import consumer from "../channels/consumer"
+
+export default class extends Controller {
+  static values = { chatroomId: Number }
+  static targets = ["messages"]
+
+  connect() {
+    console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  connect() {
+    this.channel = consumer.subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data)
+      }
+    )
+    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  // #insertMessageAndScrollDown(data) {
+  //   this.messagesTarget.insertAdjacentHTML("beforeend", data)
+  //   this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  //   }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+}
